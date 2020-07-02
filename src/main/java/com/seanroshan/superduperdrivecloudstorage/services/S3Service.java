3220f8d856ab35a4547c7de65ebb08504105d137
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-07-02 03:46:02 -0700

    feat: S3 service is ready to configure for the future

---
+++ src/main/java/com/seanroshan/superduperdrivecloudstorage/services/S3Service.java
@@ -0,0 +1,33 @@
+package com.seanroshan.superduperdrivecloudstorage.services;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectListing;
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3Service {
+
+    String bucketName = "super.duper.drive.cloud.storage";
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+            "",
+            ""
+    );
+
+    AmazonS3 s3client = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.US_WEST_2)
+            .build();
+
+    public ObjectListing listObjects(String folderKey) {
+        return s3client.listObjects(bucketName, folderKey);
+    }
+
+
+}
